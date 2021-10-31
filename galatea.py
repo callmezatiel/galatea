8cef00bb604c9162bdfde7406eeb0c9c515d2316
Zatiel <44403468+callmezatiel@users.noreply.github.com>
2021-10-31 05:24:11 -0600

    Update galatea.py

--- galatea.py
+++ galatea.py
@@ -1,11 +1,11 @@
 import socket
 host = input("Insert IP ")
 ports = range  (1,1024)
-for port in ports:
+for port in ports :
      s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
      result = s.connect_ex ((host,port))
      if result == 0:
-         print ("port " + str(port) + " is open ")
+         print ("Port " + str(port) + " is open ")
      else:
-         print ("port " + str(port) + " is close")
-        s.close()
+         print ("Port " + str(port) + " is close ")
+     s.close()
